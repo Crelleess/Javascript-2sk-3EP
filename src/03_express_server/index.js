95f2a9242545f7de17283b9807b7e1b3c87055a7
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-09-26 12:52:26 +0200

    Custom odpovědi na custom URL

--- src/03_express_server/index.js
+++ src/03_express_server/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static('www'));
 
+// custom odpovedi na custom URL v dotazu
+app.get('/hello', (dotaz, odpoved) => odpoved.send('Hello World!'));
+app.get('/json', (dotaz, odpoved) => odpoved.json({ pozdrav: 'Hello World!' }));
+
 app.listen(8000, () => {
     console.log('Server běží na http://localhost:8000...');
 });
